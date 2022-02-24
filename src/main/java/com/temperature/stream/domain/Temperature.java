c3e38dd8840d6e97b293f07b87a7cbc31d78a1cd
woqls22 <woqls226@gmail.com>
2022-02-24 13:46:09 +0900

    lombok 엑세스레벨 지정

--- src/main/java/com/temperature/stream/domain/Temperature.java
+++ src/main/java/com/temperature/stream/domain/Temperature.java
@@ -1,8 +1,11 @@
 package com.temperature.stream.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 //@Getter -> getter는 public level에서만 접근가능하다..
+@Getter(AccessLevel.PRIVATE)
 public class Temperature {
     private final double value;
 
@@ -10,7 +13,4 @@ public class Temperature {
         this.value = value;
     }
 
-    public double getValue() {
-        return value;
-    }
 }
